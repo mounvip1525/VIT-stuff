57bda3c237242e593c3cd5a7b7cf97c8eb209e1d
Mounvi <mounvip1525@gmail.com>
2021-06-19 19:15:21 +0530

    Update cricket.java

--- cricket.java
+++ cricket.java
@@ -1,1 +1,20 @@
-
+class Cricket {
+		static double addscore() {
+			int min=0,max=110;
+			return (Math.random() * (max - min + 1)) + min; 
+		}
+	public static void main(String[] args) {
+		int[] score_arr=new int[11];
+		int i=0;
+		String j="";
+		for(i=0;i<11;i++) {
+			score_arr[i] = (int)addscore();
+		}
+		for(i=0;i<11;i++) {
+			j=(score_arr[i]<=9) ? "bad" : 
+				(score_arr[i]<=99) ? "good" :
+					"very good";
+			System.out.println("a["+i+"]: "+ j);
+		}
+	}
+}
